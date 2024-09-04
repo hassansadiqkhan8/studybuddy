65c292fb5dce47b957930e4ae900b14abb1bd4e0
hassan sadiq khan <hassansadiqkhan8@gmail.com>
2024-09-04 17:25:06 +0500

    login and registration

--- base/views.py
+++ base/views.py
@@ -7,17 +7,19 @@ from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 
 # Login Page
 def loginPage(request):
     page = "login"
+
     if request.user.is_authenticated:
         return redirect("home")
     
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -40,6 +42,24 @@ def loginPage(request):
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+# Register Page
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error ocurred during registration.")
+
+    return render(request, "base/login_register.html", {"form":form})
 
 
 # Home Page
