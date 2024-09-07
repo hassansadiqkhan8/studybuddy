cf26e8ad9f4cebe5f6ecbec20d83344c713a333e
hassan sadiq khan <hassansadiqkhan8@gmail.com>
2024-09-07 16:53:42 +0500

    Activity and Components

--- base/models.py
+++ base/models.py
@@ -33,5 +33,8 @@ class Message(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-updated", "-created"]
+
     def __str__(self):
         return self.body[0:50]
